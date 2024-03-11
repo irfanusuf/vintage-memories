d29ef002b80e99c4ab9ade01304d6002f5627bcd
irfan usuf <irfanusuf33@gmail.com>
2024-03-12 00:22:20 +0530

    newBranch

--- src/postComponents/CommentBox.jsx
+++ src/postComponents/CommentBox.jsx
@@ -1,29 +1,67 @@
 import React from "react";
 import Loader from "../sharedComponents/Loader";
 import { FaHeart } from "react-icons/fa";
+import { SlUser } from "react-icons/sl";
 
 
 const CommentBox = (props) => {
+  
+ 
+
   return (
     <>
-     <div className="comments">
-       <h2>Comments</h2>
+      <div className="comment-box">
+        <h2>Comments</h2>
         {props.selectedPost === props.post._id ? (
-          <div>
+          <div className="comments">
             {props.post.comments.map((indvidualarrayItem, index) => (
               <div key={index} className="comment">
-                <div className="profile-pic"> <img src={indvidualarrayItem.user.profilepIcUrl} alt="no-preview"/> </div>
+                <div className="profile-pic">
+                  {indvidualarrayItem.user.profilepIcUrl ? (
+                    <img
+                      src={indvidualarrayItem.user.profilepIcUrl}
+                      alt="no-preview"
+                    />
+                  ) : (
+                    <SlUser style={{ fontSize: "20px" }} />
+                  )}
+                </div>
                 <b> {indvidualarrayItem.user.username} : </b>
                 <span> {indvidualarrayItem.comment}</span>
-                <FaHeart style={{color:"red" , position : "absolute" , right : "10"}}/>
+                <FaHeart
+                  style={{ color: "red", position: "absolute", right: "10" }}
+                />
               </div>
             ))}
           </div>
-        ) : (<Loader/>)}
-     </div>
-       
-      
-      </>
+        ) : (
+          <Loader />
+        )}
+
+        <div className="comment-input">
+          <form>
+            <input
+              type="text"
+              value={props.comment}
+              name="comment"
+              onChange={(e) => {
+                props.setComment(e.target.value);
+              }}
+            />
+
+            <button
+              onClick={(e) => {
+                props.submitComment(e, props.selectedPost);
+              }}
+              disabled={props.selectedPost === null}
+            >
+              {" "}
+              Comment{" "}
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/postComponents/GetAllPosts.jsx
+++ src/postComponents/GetAllPosts.jsx
@@ -7,17 +7,36 @@ import { LuMessageCircle } from "react-icons/lu";
 import { FaShare } from "react-icons/fa";
 import CommentBox from "./CommentBox";
 import { SlUser } from "react-icons/sl";
+import Loader from "../sharedComponents/Loader";
+import Likes from "./Likes";
 
 const GetAllPosts = () => {
   const [data, setData] = useState([]);
   const [heart, setHeart] = useState(null);
-  const [showComment, setShowComment] = useState(false);
   const [selectedPost, setSelectedPost] = useState(null);
+  const [selectedLikes, setSelectedLikes] =useState(null)
+  const [loading, setLoading] = useState(true);
+  const [text, setText] = useState("");
+  const [comment, setComment] = useState("");
+  const [commentSucess, setcommentSucess] = useState(false);
+
   const token = sessionStorage.getItem("token");
 
-  const handleComment = (postId) => {
-    setShowComment(!showComment);
-    setSelectedPost(postId);
+  const showComment = (postId) => {
+    
+      setSelectedPost(postId);
+    
+   
+    
+  };
+
+  const showLikes = (postId) => {
+    if(selectedLikes===null){
+      setSelectedLikes(postId);
+    }
+    else if(selectedLikes===postId){
+      setSelectedLikes(null);
+    }
   };
 
   const handleLike = async (postId) => {
@@ -33,113 +52,167 @@ const GetAllPosts = () => {
 
     console.log(response);
     if (response.data.message === `U Liked post _${postId}`) {
-      setHeart(postId);
+      setHeart(true)
+      
     } else if (response.data.message === `U unliked post_${postId}`) {
-      setHeart(null);
+      setHeart(false);
     }
   };
 
-  const fetchData = async () => {
+  const submitComment = async (e, postId) => {
+    e.preventDefault();
+
     try {
-      const res = await axios.get(
-        "http://localhost:4000/posts",
-        
+      const response = await axios.post(
+        `http://localhost:4000/post/comment?postId=${postId} `,
+        { comment: comment },
+
         {
           headers: {
             token: token,
           },
         }
       );
-      if (res.data.message === "Posts Found!") {
-        setData(res.data.allposts);
-        console.log(res);
+
+      if (response.data.message === "comment Added") {
+        setComment("");
+        setcommentSucess(!commentSucess);
       }
     } catch (err) {
-      console.log(err);
+      console.error(err.message);
     }
   };
 
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const res = await axios.get(
+          "http://localhost:4000/posts",
+
+          {
+            headers: {
+              token: token,
+            },
+          }
+        );
+        if (res.data.message === "Posts Found!") {
+          setData(res.data.allposts);
+          if (res.data.allposts.length === 0) {
+            setText("NO POSTS AVAILABLE!");
+          }
+        }
+      } catch (err) {
+        if (
+          err.message === "Request failed with status code 403" ||
+          "Network Error"
+        ) {
+          setLoading(false);
+        }
+        console.error(err.message);
+      }
+    };
     fetchData();
-  }, [heart]);
+  }, [heart, commentSucess]);
 
   return (
     <div className="main">
       <h1> Explore </h1>
 
       <div className="container">
-        {data.map((post) => (
-          <div className="card">
-            <div className="likes">
-              {" "}
-              <h2> Likes</h2>
-            </div>
+        {text && (
+          <h1 style={{ color: "white", marginTop: "300px" }}> {text} </h1>
+        )}
 
-            <div className="post">
-              <div className="heading">
-                <div className="profile-pic">
-                  {post.author.profilepIcUrl ? (
-                    <img src={post.author.profilepIcUrl} alt="no-preview" />
-                  ) : (
-                    <SlUser style={{ fontSize: "30px" }} />
-                  )}
+        {!loading ? (
+          <Loader />
+        ) : (
+          data.map((post) => (
+            <div className="card">
+
+
+             {selectedLikes === post._id && <Likes post={post} />} 
+
+              <div className="post">
+                <div className="heading">
+                  <div className="profile-pic">
+                    {post.author.profilepIcUrl ? (
+                      <img src={post.author.profilepIcUrl} alt="no-preview" />
+                    ) : (
+                      <SlUser style={{ fontSize: "20px" }} />
+                    )}
+                  </div>
+
+                  <b>
+                    {post && post.author
+                      ? post.author.username
+                      : "Default Author"}
+
+
+                      
+                  </b>
                 </div>
-
-                <b>
-                  {post && post.author
-                    ? post.author.username
-                    : "Default Author"}
+                <b style={{ marginTop: "10px" }}>
+                  {post && post.title ? post.title.toUpperCase() : null}
                 </b>
-              </div>
-              <b style={{ marginTop: "10px" }}>
-                {post && post.title ? post.title.toUpperCase() : null}
-              </b>
 
-              <img
-                onDoubleClick={() => {
-                  handleLike(post._id);
-                }}
-                src={post.imageUrl}
-                alt="preview"
-              />
-
-              <div className="icons">
-                <span
-                  onClick={() => {
+                <img
+                  onDoubleClick={() => {
                     handleLike(post._id);
                   }}
-                >
-                  {heart === post._id ? (
-                    <FaHeart style={{ color: "red" }} />
-                  ) : (
-                    <FaRegHeart />
-                  )}
-                </span>
+                  src={post.imageUrl}
+                  alt="preview"
+                />
 
-                <span
-                  onClick={() => {
-                    handleComment(post._id);
-                  }}
-                >
-                  <LuMessageCircle />
-                </span>
-                <span>
-                  <FaShare />
-                </span>
+                <div className="icons">
+                  <span
+                    onClick={() => {
+                      handleLike(post._id);
+                    }}
+                  >
+                      {post.author.likedPosts.indexOf( post._id ) > -1  ? <FaHeart style={{ color: "red" }} /> : <FaRegHeart /> }
+
+                    {/* {heart === post._id ? (
+                      <FaHeart style={{ color: "red" }} />
+                    ) : (
+                      <FaRegHeart />
+                    )} */}
+                  </span>
+
+                  <span
+                    onClick={() => {
+                      showComment(post._id);
+                    }}
+                  >
+                    <LuMessageCircle />
+                  </span>
+                  <span>
+                    <FaShare />
+                  </span>
+                </div>
+
+                <div className="counts">
+                  <span>{post.likeCounts.length}</span>
+                  <span>{post.comments.length}</span>
+                  <span>{post.shareCounts.length}</span>
+                  <span   onClick={() => {
+                      showLikes(post._id);
+                    }} > Show likes </span>
+                </div>
+
+                <b> {post.caption}</b>
               </div>
 
-              <div className="counts">
-                <span>{post.likeCounts.length}</span>
-                <span>{post.comments.length}</span>
-                <span>{post.shareCounts.length}</span>
-              </div>
-
-              <b> {post.caption}</b>
+              <CommentBox
+                post={post}
+                selectedPost={selectedPost}
+                submitComment={submitComment}
+                comment={comment}
+                setComment={setComment}
+              />
             </div>
-
-            <CommentBox post={post} selectedPost={selectedPost} />
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
