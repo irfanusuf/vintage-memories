df5cd09661a1bced1591de5b3057ba0fba08ff92
unknown <irfanusuf33@gmail.com>
2024-03-18 14:12:37 +0530

    addedProfile

--- src/postComponents/Profile.jsx
+++ src/postComponents/Profile.jsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import "./Profile.css";
 
 const Profile = () => {
   const { userId } = useParams();
@@ -15,27 +16,56 @@ const Profile = () => {
 
       if (res.data.message === "user Found") {
         setData(res.data.user);
+        console.log(res);
       } else {
         console.log("srver Error");
       }
     } catch (err) {
       console.log(err);
     }
-  }
- , [userId])
- 
- 
+  }, [userId]);
+
   useEffect(() => {
     fetchUser();
   }, [fetchUser]);
 
   return (
-    <div>
-      <img src={data.profilepIcUrl} alt="no" width={300} />
-      <p> {data.username} </p>
-      {/* <p>  {data.userFollowers.length} </p> */}
-      {/* <p>  {data.userFollowing.length} </p> */}
-      {/* <p>  {data.posts.length} </p> */}
+    <div className="profile ">
+      <div className="profile-container">
+        <div className="profile-heading">
+          <div className="container-left">
+            <img src={data.profilepIcUrl} alt="no" width={100} />
+            <p> {data.username} </p>
+          </div>
+
+          <div className="container-right">
+            <div className="headings">
+              <span> Posts</span>
+              <span> Followers</span>
+              <span> Following </span>
+            </div>
+
+            <div className="counts">
+              {" "}
+              {/* <p> {data.posts.length} </p> */}
+              {/* <p> {data.userFollowers.length} </p> */}
+              {/* <p> {data.userFollowing.length} </p> */}
+            </div>
+          </div>
+        </div>
+
+        <div className="post-container">
+
+
+         
+          {data.posts.map((singleElement) => (
+            <div className="single-post">
+            
+              <img src= {singleElement.post.imageUrl}  width={180} alt="no-preveiew"/>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
